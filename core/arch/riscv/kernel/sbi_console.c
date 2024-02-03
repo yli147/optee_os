dfd9d8a7a69aa00cb2ae8e5343f5e4a1a6585e4a
Yu Chien Peter Lin <peterlin@andestech.com>
2024-02-03 10:32:08 +0800

    riscv: sbi_console: Cleanup SBI console driver
    Remove the following:
    - #ifdef preprocessor directive as sbi_console.c is conditionally
      assigned based on CFG_RISCV_SBI_CONSOLE in sub.mk.
    - sbi_console_global_lock as the sbi_console_putc() has been protected
      by puts_lock.
    - The empty sbi_console_flush().
    
    Split FID 0 from SBI_EXT_0_1_CONSOLE_PUTCHAR definition for better
    readability. Also, provide a function description.
    
    Signed-off-by: Yu Chien Peter Lin <peterlin@andestech.com>

--- core/arch/riscv/kernel/sbi_console.c
+++ core/arch/riscv/kernel/sbi_console.c
@@ -14,39 +14,19 @@
 #include <trace.h>
 #include <util.h>
 
-#ifdef CFG_RISCV_SBI_CONSOLE
-
 struct sbi_console_data {
 	struct serial_chip chip;
 };
 
 static struct sbi_console_data console_data __nex_bss;
-static unsigned int sbi_console_global_lock __nex_bss = SPINLOCK_UNLOCK;
-
-static void sbi_console_lock_global(void)
-{
-	cpu_spin_lock(&sbi_console_global_lock);
-}
-
-static void sbi_console_unlock_global(void)
-{
-	cpu_spin_unlock(&sbi_console_global_lock);
-}
-
-static void sbi_console_flush(struct serial_chip *chip __unused)
-{
-}
 
 static void sbi_console_putc(struct serial_chip *chip __unused,
 			     int ch)
 {
-	sbi_console_lock_global();
 	sbi_console_putchar(ch);
-	sbi_console_unlock_global();
 }
 
 static const struct serial_ops sbi_console_ops = {
-	.flush = sbi_console_flush,
 	.putc = sbi_console_putc,
 };
 
@@ -60,6 +40,3 @@ void console_init(void)
 	sbi_console_init(&console_data);
 	register_serial_console(&console_data.chip);
 }
-
-#endif /*CFG_RISCV_SBI_CONSOLE*/
-
